c87aa60259fffeeb7e025fa6cb09586d21ba0ecc
azuspan607 <aa.zuspan@gmail.com>
2020-01-17 16:26:54 -0800

    Refactor to pre-instantiate and store instances of all commands

--- DjangoAssistant/CommandHandler.py
+++ DjangoAssistant/CommandHandler.py
@@ -10,17 +10,28 @@ class CommandHandler:
     COMMAND_DIR = 'commands'
 
     def __init__(self):
+        self.cmd_list = self.get_commands()
         self.cmds = self.load_commands()
 
-    def load_commands(self):
+    def get_commands(self):
         """
         Find all commands in the command directory and import them. Then return all of them that are subclasses of
-        Command
+        Command in a list
         :return : A list of classes which are subclasses of Command
         """
         for (module_loader, name, ispkg) in pkgutil.iter_modules([self.COMMAND_DIR]):
             importlib.import_module('DjangoAssistant.commands.' + name, __package__)
         return Command.__subclasses__()
+
+    def load_commands(self):
+        """
+        Instantiate all commands and store them for later use
+        :return:
+        """
+        loaded_cmds = []
+        for cmd in self.cmd_list:
+            loaded_cmds.append(cmd(user_input=None))
+        return loaded_cmds
 
     def respond(self, user_input):
         """
@@ -29,9 +40,13 @@ class CommandHandler:
         :return: True if command was handled, false if no commands were recognized
         """
         for cmd in self.cmds:
-            cmd_obj = cmd(user_input)
+            cmd.user_input = user_input
 
-            if cmd_obj.recognize():
-                cmd_obj.respond()
+            if cmd.recognize():
+                cmd.respond()
                 return True
         return False
+
+    # def help(self):
+    #     for cmd in self.cmds:
+    #         print(cmd.recognized_commands[0])
