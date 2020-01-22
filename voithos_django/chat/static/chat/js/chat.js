650ef8f44edaa7a3082a7b3c00fda9ebff0ed6fd
azuspan607 <aa.zuspan@gmail.com>
2020-01-22 14:50:18 -0800

    Move timeout into addMessage func and add intro message

--- voithos_django/chat/static/chat/js/chat.js
+++ voithos_django/chat/static/chat/js/chat.js
@@ -2,6 +2,13 @@ const USER = 'user';
 const VOITHOS = 'voithos';
 const MESSAGE_BOX = document.getElementById('message-container');
 
+
+// When chat is submitted, handle it
+document.getElementById('submit-button').addEventListener('click', handleChatInput)
+
+addMessage('Hello! I am Voithos. What can I do for you?', VOITHOS);
+
+
 // Handle chat input by making a GET request with the data and receiving and displaying the response
 function handleChatInput() {
     const input_form = document.getElementById('input-text')
@@ -21,21 +28,23 @@ function handleChatInput() {
         // If a server response is received, display the messages
         success: function (data) {
             addMessage(input_text, USER);
-
-            // Delay Voithos' response to feel more natural
-            setTimeout(function () { addMessage(data['output'], VOITHOS) }, 750);
+            addMessage(data['output'], VOITHOS)
         }
     })
 }
 
 // Add a message to the message box. Message class based on whether it's sent by user or voithos
 function addMessage(message, sender) {
-    MESSAGE_BOX.innerHTML += `<div class='msg msg-${sender}'>${message}</div>`;
+    if (sender == VOITHOS) {
+        // Voithos responses should be slightly delayed for readability
+        setTimeout(function () { MESSAGE_BOX.innerHTML += `<div class='msg msg-${sender}'>${message}</div>` }, 750);
+    }
+    else {
+        MESSAGE_BOX.innerHTML += `<div class='msg msg-${sender}'>${message}</div>`;
+    }
     scrollToBottom();
 }
 
-// When chat is submitted, handle it
-document.getElementById('submit-button').addEventListener('click', handleChatInput)
 
 
 // Automatically scroll to bottom of message box
