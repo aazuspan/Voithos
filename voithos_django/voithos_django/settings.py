17d85f23426784335c8bab365ec3637a25dc3291
azuspan607 <aa.zuspan@gmail.com>
2020-01-24 12:29:34 -0800

    Improve navbar styling, add device-aware width

--- voithos_django/voithos_django/settings.py
+++ voithos_django/voithos_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.7.34', '127.0.0.1']
 
 
 # Application definition
