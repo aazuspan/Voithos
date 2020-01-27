b6fbb80b9c71b97e30eec5eb6493def16d9dc06e
azuspan607 <aa.zuspan@gmail.com>
2020-01-26 16:48:33 -0800

    Delete existing trained engine when retraining

--- voithos_django/Voithos/utilities/train_commands.py
+++ voithos_django/Voithos/utilities/train_commands.py
@@ -1,5 +1,6 @@
 import json
 import os
+import shutil
 from snips_nlu import SnipsNLUEngine
 from snips_nlu.default_configs import CONFIG_EN
 from Voithos.CommandHandler import CommandHandler
@@ -8,12 +9,16 @@ from Voithos.CommandHandler import CommandHandler
 def main():
     """
     This builds a training dataset, trains an NLU engine with it, and saves that engine. This must be done any time a
-    new command is added or utterances are edited for a command.
+    new command is added or utterances are edited for a command. If an NLU engine already exists, it is deleted.
     """
     training_json = json.loads(build_training_dataset())
+    engine_path = os.path.join('Voithos', 'utilities', 'NLU')
+
+    if os.path.exists(engine_path):
+        shutil.rmtree(engine_path)
+
     nlu_engine = SnipsNLUEngine(config=CONFIG_EN)
     nlu_engine = nlu_engine.fit(training_json)
-    engine_path = os.path.join('Voithos', 'utilities', 'NLU')
     nlu_engine.persist(engine_path)
 
 
@@ -23,19 +28,20 @@ def build_training_dataset():
     :return: A Json of training data built from all commands
     """
     cmd_handler = CommandHandler(voithos=None)
-    cmd_dict = {'entities': {}, 'intents': {}, 'language': 'en'}
+    cmd_dict = {"entities": {}, "intents": {}, "language": "en"}
 
     for cmd in cmd_handler.cmd_list:
         try:
-            cmd_dict['intents'][cmd.name] = {'utterances': [{'data': []}]}
+            cmd_dict['intents'][cmd.name] = {"utterances": [{"data": []}]}
 
             for utterance in cmd.utterances:
                 # This is a nightmare.
-                cmd_dict['intents'][cmd.name]['utterances'][0]['data'].append({'text': utterance})
+                cmd_dict['intents'][cmd.name]['utterances'][0]['data'].append({"text": utterance})
         except AttributeError:
             pass
 
     training_json = json.dumps(cmd_dict)
+
     return training_json
 
 
