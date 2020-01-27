0435dd088983672262da21ce84afa9b563b6b57e
azuspan607 <aa.zuspan@gmail.com>
2020-01-27 15:57:07 -0800

    Remove unused sys path insert

--- voithos_django/scripts/run_tests.py
+++ voithos_django/scripts/run_tests.py
@@ -1,9 +1,7 @@
 import os
-import sys
 import unittest
 
 TEST_DIR = os.path.join('tests')
-sys.path.insert(0, os.path.join('voithos_django'))
 
 loader = unittest.TestLoader()
 suite = loader.discover(TEST_DIR)
