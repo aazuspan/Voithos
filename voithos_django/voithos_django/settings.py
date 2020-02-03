7953f362d024c416e7fafbee21ce381e8c32580e
azuspan607 <aa.zuspan@gmail.com>
2020-02-03 10:08:53 -0800

    Add heroku to allowed hosts

--- voithos_django/voithos_django/settings.py
+++ voithos_django/voithos_django/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.7.34', '127.0.0.1']
+ALLOWED_HOSTS = ['hellovoithos.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
