5ac99e9b2bfff5ed739a6f5b756959792cafccfc
azuspan607 <aa.zuspan@gmail.com>
2020-02-05 18:18:16 -0800

    Start implementing response feedback handling - INCOMPLETE

--- voithos_django/static/chat/js/chat.js
+++ voithos_django/static/chat/js/chat.js
@@ -37,20 +37,56 @@ function handleChatInput() {
             const end_time = new Date().getTime();
             // Subtract time it took to get response from delay time
             const delay_time = Math.max(RESPONSE_DELAY_MS - (end_time - start_time), 0);
-            addMessage(data['output'], VOITHOS, delay_time)
+            addMessage(data['output'], VOITHOS, delay_time, null, data['input']);
         }
     })
 }
 
+// When a response feedback button is clicked, hide the input text that triggered the response in the feedback modal
+$(document).on('click', '.button-review', function (e) {
+    addHiddenInputToModal(e);
+});
+
+// Enable feedback submit button when a choice is made
+$('#command-choices').on('change', function () {
+    $('#feedback-submit').prop('disabled', false);
+})
+
+// When response feedback is submitted, handle it
+$('#feedback-submit').on('click', function (e) {
+    handleResponseFeedback(e);
+});
+
+// TODO: Fix issue with jquery returning n.fn.init, forcing me to select by index 0
+// When a response is reviewed, pass the input that triggered it to the modal
+function addHiddenInputToModal(event) {
+    responseInput = $(event.target).closest('.msg-voithos').find('.response-input')[0].innerText;
+    $('#response-input').text(responseInput);
+}
+
+// TODO: send submitted choice and associated user input via Ajax, handle them in Python
+function handleResponseFeedback(e) {
+    const submitted_choice = $('#command-choices option:selected').text();
+}
+
+
 // Add a message to the message box. Message class based on whether it's sent by user or voithos
-function addMessage(message, sender, delay = 0, first = false) {
-    let btn_template = "<button class='btn btn-sm float-right button-review' data-toggle='modal' data-target='#feedbackModal' title='Mark incorrect response'><i class='fa fa-times'></i></button>"
+function addMessage(message, sender, delay = 0, first = false, input = null) {
+    let btn_template = "<button class='btn btn-sm float-right button-review' data-toggle='modal' data-target='#feedback-modal' title='Mark incorrect response'><i class='fa fa-times'></i></button>"
+
+    // Input that prompted the response, if any
+    let input_template = '';
+    if (input) {
+        // Hide it in the message so it can be retrieved for NLU training
+        input_template = `<div class="response-input" hidden>${input}</div>`;
+    }
+
     // User messages and first message don't need button to report incorrect response
     if (sender == USER || first == true) {
         btn_template = '';
     }
     setTimeout(function () {
-        MESSAGE_BOX.innerHTML += `<div class='msg msg-${sender}'>${message}${btn_template}</div>`;
+        MESSAGE_BOX.innerHTML += `<div class='msg msg-${sender}'>${message}${btn_template}${input_template}</div>`;
         scrollToBottom();
     }, delay);
 }
