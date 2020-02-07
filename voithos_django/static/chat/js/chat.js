694b0b4a57a258e24307af53da801f4fd21f648d
azuspan607 <aa.zuspan@gmail.com>
2020-02-06 21:06:52 -0800

    Add and implement message queue system

--- voithos_django/static/chat/js/chat.js
+++ voithos_django/static/chat/js/chat.js
@@ -3,16 +3,71 @@ const VOITHOS = 'voithos';
 const MESSAGE_BOX = document.getElementById('message-container');
 const RESPONSE_DELAY_MS = 750;
 
+class MessageQueue {
+    constructor() {
+        // List of messages to post
+        this.queue = [];
+        // Currently waiting to poste
+        this.active = false;
+    }
+
+    // Add a message to queue
+    addMessage(message) {
+        this.queue.push(message);
+        this.checkStatus();
+    }
+
+    // Check if a message needs to be posted and isn't currently in progress
+    checkStatus() {
+        if (!this.active && this.queue.length > 0) {
+            this.activate();
+        }
+    }
+
+    // Start process of posting message
+    activate() {
+        this.active = true;
+        let nextMessage = this.queue.shift();
+
+        setTimeout(() => {
+            this.postMessage(nextMessage);
+        }, nextMessage.delay);
+    }
+
+    // Add the message to the page and reset the process
+    postMessage(message) {
+        message.post();
+        this.active = false;
+        this.checkStatus();
+    }
+}
+
+class Message {
+    constructor(message, sender, delay = 0) {
+        this.message = message;
+        this.sender = sender;
+        this.delay = delay;
+    }
+
+    // Add the message to the page
+    post() {
+        MESSAGE_BOX.innerHTML += `<div class='msg msg-${this.sender}'>${this.message}</div>`;
+        scrollToBottom();
+    }
+}
+
+const queue = new MessageQueue();
 
 // When chat is submitted, handle it
 document.getElementById('submit-button').addEventListener('click', handleChatInput)
+
 intro();
 
 // Voithos introduction for new user
 function intro() {
     let inputGroupWrapper = document.getElementById('input-group-wrapper')
-    addMessage('Hello! I am Voithos. I am a personal assistant that uses machine learning and artificial intelligence to perform tasks for you.', VOITHOS, 1000);
-    addMessage('You can give me commands or ask me questions by typing in the form at the bottom of the screen and pressing send. For example, you could say "Tell me a joke, Voithos!"', VOITHOS, 2500);
+    queue.addMessage(new Message('Hello! I am Voithos. I am a personal assistant that uses machine learning and artificial intelligence to perform tasks for you.', VOITHOS, 1000));
+    queue.addMessage(new Message('You can give me commands or ask me questions by typing in the form at the bottom of the screen and pressing send. For example, you could say "Tell me a joke, Voithos!"', VOITHOS, 1500));
 
     // Add a blue highlight to the text box that disappears when clicked
     setTimeout(function () {
@@ -22,7 +77,7 @@ function intro() {
         }, { once: true });
     }, 3000);
 
-    addMessage('If you\'re not sure what to ask me, you can type "help" to get a list of commands I will recognize.', VOITHOS, 7000);
+    queue.addMessage(new Message('If you\'re not sure what to ask me, you can type "help" to get a list of commands I will recognize.', VOITHOS, 4500));
 }
 
 
@@ -42,7 +97,7 @@ function handleChatInput() {
         return;
     }
 
-    new Message(input_text, USER).post();
+    queue.addMessage(new Message(input_text, USER));
 
     // Send the form data in a GET request
     $.ajax({
@@ -58,7 +113,7 @@ function handleChatInput() {
             const end_time = new Date().getTime();
             // Subtract time it took to get response from delay time
             const delay_time = Math.max(RESPONSE_DELAY_MS - (end_time - start_time), 0);
-            new Message(data['output'], VOITHOS, delay_time).post();
+            queue.addMessage(new Message(data['output'], VOITHOS, delay_time));
         }
     })
 }
@@ -79,16 +134,6 @@ function scrollToBottom() {
     });
 }
 
-class Message {
-    constructor(message, sender, delay = 0) {
-        this.message = message;
-        this.sender = sender;
-        this.delay = delay;
-    }
 
-    // Add the message to the page
-    post() {
-        MESSAGE_BOX.innerHTML += `<div class='msg msg-${this.sender}'>${this.message}</div>`;
-        scrollToBottom();
-    }
-}
+
+
