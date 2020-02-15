852a9bde93f27691dcd6d812dd296eb76fc2a4bc
azuspan607 <aa.zuspan@gmail.com>
2020-02-14 21:49:30 -0800

    Replace internal docs link with github link and remove about link

--- voithos_django/chat/views.py
+++ voithos_django/chat/views.py
@@ -3,7 +3,6 @@ from django.http import JsonResponse, HttpResponse
 from Voithos.Voithos import Voithos
 
 
-# TODO: Refactor to allow Voithos to persist between calls so that chat history could be preserved
 def chat(request):
     """
     Handle the chat page. If a GET request is received with input text, pass it to Voithos and return the Json response
@@ -14,13 +13,3 @@ def chat(request):
         return JsonResponse({'output': response, 'delay': response_delay})
 
     return render(request, 'chat/index.html')
-
-
-# TODO: Create and pass About template
-def about(request):
-    return HttpResponse('<h1> ABOUT </h1>')
-
-
-# TODO: Create and pass Documentation template
-def documentation(request):
-    return HttpResponse('<h1> DOCUMENTATION </h1>')
