ab14d7351e08b009a32138fda866b7963e5c9abc
azuspan607 <aa.zuspan@gmail.com>
2020-02-16 14:27:47 -0800

    Add dark/light theme functionality and styling

--- voithos_django/static/chat/js/chat.js
+++ voithos_django/static/chat/js/chat.js
@@ -171,3 +171,16 @@ function scrollToBottom() {
         behavior: 'smooth'
     });
 }
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
