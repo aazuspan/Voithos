aae929c63797d764d319dc5bda0ceb4b7e29cd89
azuspan607 <aa.zuspan@gmail.com>
2020-03-05 12:34:48 -0800

    Add auto scroll to bottom of message list

--- frontend/gui/src/containers/MessageList.js
+++ frontend/gui/src/containers/MessageList.js
@@ -3,18 +3,46 @@ import React from 'react';
 import Message from '../components/Message';
 import TypingIndicator from '../components/TypingIndicator';
 
-function MessageList(props) {
-    const messages = props.messages.map((msg) =>
-        <Message key={props.messages.indexOf(msg)} content={msg.content} sender={msg.sender} />);
 
-    let indicator = props.showTypingIndicator ? <TypingIndicator /> : null;
+class MessageList extends React.Component {
+    constructor() {
+        super();
+        this.listEndRef = React.createRef();
+    }
 
-    return (
-        <div className="msg-container">
-            {messages}
-            {indicator}
-        </div>
-    )
+    componentDidMount() {
+        this.scrollToBottom();
+    }
+
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
+
+    scrollToBottom = () => {
+        this.listEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    render() {
+        this.messages = this.props.messages.map((msg) =>
+            <Message
+                key={this.props.messages.indexOf(msg)}
+                content={msg.content}
+                sender={msg.sender} />);
+
+        this.indicator = this.props.showTypingIndicator
+            ?
+            <TypingIndicator />
+            :
+            null;
+
+        return (
+            <div className="msg-container">
+                {this.messages}
+                {this.indicator}
+                <div ref={this.listEndRef} />
+            </div>
+        )
+    }
 }
 
 export default MessageList;
